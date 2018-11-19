28cf04d759aab750687b4b89579b3c2359908a34
Thomas van den Berg <30773643+ThomasvandenbergCode@users.noreply.github.com>
2018-11-19 12:17:49 +0100

    Add files via upload

---
+++ colorPlugin.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  WordPress.org colorPlugin
+Plugin URI:   https://developer.wordpress.org/plugins/the-basics/
+Description:  Deze plugin zorgt ervoor dat  alle woorden op je website willekeurige kleuren toegewezen krijgen, dit voor een leuke sfeer.
+Version:      1.0
+Author:       ThomasvandenBerg
+Author URI:   https://developer.wordpress.org/
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wporg
+Domain Path:  /languages
+*/
